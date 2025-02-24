da6628b2a268c14ff572e6b2f31e53caa110531d
PedroGuilhermeIsidoro <2300548@fortecpraiagrande.com.br>
2025-02-24 19:33:15 -0300

    Add files via upload

---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,20 @@
+package javaapplication3;
+
+import java.util.Scanner;
+public class JavaApplication3 {
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] salve = new int[10];
+        int soma = 0;    
+        for(int i=0;i<salve.length;i++){
+        System.out.println(" me diga a altura em cm");
+        salve[i] = scan.nextInt();
+        soma += salve[i];
+        }
+        soma = soma/salve.length;
+        System.out.println(soma + "cm");
+    }
+   
+}
